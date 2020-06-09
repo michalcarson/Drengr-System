a6afa6f8f188139095f5a6397ad15527ec153554
Michal Carson <michal.carson@gmail.com>
2020-06-08 23:08:48 -0500

    ensure we aren't always setting a default of false

--- Framework/Option.php
+++ Framework/Option.php
@@ -5,16 +5,23 @@ namespace Drengr\Framework;
 class Option
 {
     /**
+     * Retrieve a value from WP options storage.
+     *
      * @param string $option
-     * @param false $default
+     * @param mixed $default
      * @return mixed
      */
     public function get($option, $default = false)
     {
-        return get_option($option, $default);
+        if (func_num_args() > 1) {
+            return get_option($option, $default);
+        }
+        return get_option($option);
     }
 
     /**
+     * Store a value in WP options storage.
+     *
      * @param string $option
      * @param mixed $value
      * @return bool false if the value was not updated
