69fa31e566494374e905d4931c4731befef56948
Michal Carson <michal.carson@gmail.com>
2020-07-01 12:21:34 -0500

    respond to undefined methods in BaseController

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -2,7 +2,19 @@
 
 namespace Drengr\Controller;
 
-class BaseController
+abstract class BaseController
 {
-
+    public function __call($name, $args)
+    {
+        return [
+            'respond_to' => [
+                'json' => [
+                    'error' => sprintf('`%s` method is not defined.', $name)
+                ],
+                'html' => function () {
+                    include get_404_template();
+                }
+            ]
+        ];
+    }
 }
